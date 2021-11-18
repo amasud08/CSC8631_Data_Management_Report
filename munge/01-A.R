e1524362e31a7ad6babeb899fd090846794be4a6
amasud08 <a.masud2@newcastle.ac.uk>
2021-11-18 18:29:13 +0000

    Exploring different parts of the enrolment data inc. time, country and age over different runs

--- munge/01-A.R
+++ munge/01-A.R
@@ -38,3 +38,65 @@ ggplot(num_of_students_per_run, aes(x=run, y=n)) +
   theme_light() +
   labs(title = "Number of student enrollments by session",
        x = "Session", y = "Numbers of students")
+
+# Where are the majority of students situated?
+countries <- all_enrolments %>%
+  mutate_if(is.factor, as.character) %>%
+  select(detected_country) %>%
+  count(detected_country) %>%
+  arrange(desc(n))
+
+countries %>% 
+  select(n) %>%
+  max()
+which.max(countries$n == 11663)   # answer was GB
+# countries[65, ]
+
+# for country list: 
+# https://www.datahub.io/core/country-list#r
+
+
+### What's the percentage of students that give their details during enrollment?
+student_details_age <- all_enrolments %>%
+  group_by(run, age_range) %>%
+  count()
+
+# removing unknown students data
+student_age_NA <- student_details_age %>%
+  mutate(age_range = replace(age_range, age_range == "Unknown", NA)) %>%
+  drop_na()
+
+ggplot(student_age_NA, aes(x=age_range, y=n)) +
+  geom_point(aes(colour=run)) +
+  scale_colour_manual("blue", "red", "pink", "purple", "orange", "green", "yellow") +
+  theme_minimal()
+
+# scale_x_continuous 
+
+# What time of day did students typically participate in the course?
+date_time <- all_enrolments %>%
+  select(enrolled_at, run, detected_country) %>%
+  separate(enrolled_at, c("date", "time", "UTC"), sep=" ")
+
+organise_times <- date_time %>%
+  group_by(run) %>%
+  arrange(run, time) %>%
+  mutate_if(is.character, as.factor)
+
+plot(date_time)
+
+ggplot(organise_times, aes(x=run, y=time)) +
+  geom_line()
+
+install.packages("lubridate")
+library(lubridate)
+
+date_time %>%
+  group_by(run) %>%
+  ggplot(aes(time)) +
+  geom_freqpoly(binwidth = 86400)
+
+
+
+
+
