bfc212451ba61e9783a06b63b6acdebe7664142e
amasud08 <a.masud2@newcastle.ac.uk>
2021-11-25 16:55:22 +0000

    Changed preprocessing file to add column to individual datasets and then join together

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,8 +1,14 @@
-# Example preprocessing script.
+setwd("~/Desktop/CSC8631_Report")
 
-library(ProjectTemplate)
-setwd("~/Desktop")
-create.project("CSC8631_Report")
-setwd("CSC8631_Report/reports")
+# Add another column to the data to differentiate between the different datasets 
+cyber.security.1_enrolments$run = 1
+cyber.security.2_enrolments$run = 2
+cyber.security.3_enrolments$run = 3
+cyber.security.4_enrolments$run = 4
+cyber.security.5_enrolments$run = 5
+cyber.security.6_enrolments$run = 6
+cyber.security.7_enrolments$run = 7
 
-tinytex::install_tinytex()
+# joining data together 
+pasted_enrolments <- mget(paste0("cyber.security.", 1:7, "_enrolments"))
+all_enrolments <- reduce(pasted_enrolments, full_join)
