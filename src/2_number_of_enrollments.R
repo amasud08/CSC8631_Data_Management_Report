b1abae32a48631be53e31980887a67c676ebe8d6
amasud08 <a.masud2@newcastle.ac.uk>
2021-11-25 17:20:15 +0000

    Edited plot of enrolments and added final plot as image in graph folder

--- src/2_number_of_enrollments.R
+++ src/2_number_of_enrollments.R
@@ -7,12 +7,12 @@ num_of_students_per_run <- all_enrolments %>%
 # Plotting the data to see what it shows
 plot_num_of_students <- ggplot(num_of_students_per_run, aes(x=run, y=n)) +
   geom_line() +                        # including a line to highlight 
-  geom_point(colour="deeppink1") +     # changing the colour of the points 
+  geom_point(colour="deeppink1", size=3) +     # changing the colour of the points 
   xlim(0, 7) +                         # setting the limits of the x axis
   ylim(0, 15000) +                     # setting the limits of the y axis
   theme_light() +                      # want a white background 
-  labs(title = "Number of student enrollments per run",
-       x = "Run", y = "Numbers of students") 
+  labs(title = "Number of student enrolments per run",
+       x = "Run", y = "Number of students") 
 
 plot_num_of_students
 
