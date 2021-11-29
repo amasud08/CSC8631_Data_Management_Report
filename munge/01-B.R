397be12ec43b1c8811806c7a753ce1fca6965f78
amasud08 <a.masud2@newcastle.ac.uk>
2021-11-29 15:36:02 +0000

    removing install packages and added packages to my global config file instead

--- munge/01-B.R
+++ munge/01-B.R
@@ -1,9 +1,9 @@
 # Installing countrycode to add country and region to the data 
-install.packages("countrycode")   # install the package 
-library(countrycode)              # load the library
-
-# installing ggrepel to help with labeling on plots and points
-install.packages("ggrepel")
-library(ggrepel)
+# install.packages("countrycode")   # install the package 
+# library(countrycode)              # load the library
+# 
+# # installing ggrepel to help with labeling on plots and points
+# # install.packages("ggrepel")
+# library(ggrepel)
 
 # # had to install.packages("Rcpp") separately as was having problems with my laptop
